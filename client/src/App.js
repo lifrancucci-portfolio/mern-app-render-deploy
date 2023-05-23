8fd9eba68562ee8ef3835e77b6bc002064f5855e
Lisandro Francucci <lisandro.francucci@gmail.com>
2023-05-23 15:44:38 -0300

    Configure backend and frontend for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://mern-app-render-deploy.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
