e1fd533ef5e050b73ce969fe4530c0bbefe2e8ff
Lisandro Francucci <lisandro.francucci@gmail.com>
2023-05-23 15:51:41 -0300

    Prepare for deployment

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 // middleware
 const corsOptions = {
-    origin: "http://localhost:3000" // frontend URI (ReactJS)
+    origin: "https://mern-app-frontend-3zya.onrender.com" // frontend URI (ReactJS)
 }
 app.use(express.json());
 app.use(cors(corsOptions));
